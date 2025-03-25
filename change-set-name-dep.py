1e2f7ba7f4a4662b0ad6ec036b1e08e2909fe5d4
OscarCaro0328 <oscar.ricardo.caro@gmail.com>
2025-03-25 17:58:39 -0500

    DT standard mapping updated

--- change-set-name-dep.py
+++ change-set-name-dep.py
@@ -25,7 +25,7 @@ failover_list = []
 
 # Channel ID to Name mapping
 channel_name_map = {
-    "1": "Drive Thru - Vertical -",
+    "1": "Drive Thru -",
     "3": "Coates-System -",
     "4": "DT DNK Presell -",
     "5": "FC DNK -",
